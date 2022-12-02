f646ae693622b196017c5fbd6faa530502cb9cfe
emerymastering <emery@mastering.lt>
2022-12-02 01:28:27 +0100

    another solution added

--- day3/power.js
+++ day3/power.js
@@ -4,11 +4,13 @@ import fs from "fs";
 
 const input = fs.readFileSync("./input.txt", "utf8").split("\n");
 
+// console.log(input);
+
 let count = {};
 
 for (let line of input) {
   for (let i = 0; i < line.length; i++) {
-    console.log(count[i]);
+    // console.log(count[i]);
     if (!count[i]) {
       // Initialize [zeros_count, ones_count]
       count[i] = [0, 0];
@@ -17,11 +19,14 @@ for (let line of input) {
     let val = +line[i];
 
     count[i][val]++;
+    console.log("count", count[i]);
   }
 }
 
 let mins = [];
 let maxs = [];
+// console.log(count);
+// console.log(Object.values(count));
 for (let counts of Object.values(count)) {
   if (counts[0] > counts[1]) {
     maxs.push(0);
@@ -31,8 +36,10 @@ for (let counts of Object.values(count)) {
     mins.push(0);
   }
 }
-
+// console.log(mins, maxs);
 let min = parseInt(mins.join(""), 2);
 let max = parseInt(maxs.join(""), 2);
+
+// console.log(min, max);
 
 console.log(min * max);
---
+++ day3/power2.js
@@ -0,0 +1,55 @@
+// const dataObj = {
+//   1: "101010101",
+//   2: "110101010",
+//   3: "111011111",
+// };
+
+import fs from "fs";
+
+// Importing data from file
+
+const input = fs.readFileSync("./input.txt", "utf8").split("\n");
+
+const dataObj = Object.assign({}, input);
+
+console.log(dataObj);
+
+let countObj = {};
+
+for (let line of Object.values(dataObj)) {
+  for (let i = 0; i < line.length; i++) {
+    if (!countObj[i]) {
+      // Initialize [zeros_count, ones_count]
+      countObj[i] = [0, 0];
+    }
+    if (line[i] === "1") {
+      countObj[i][0]++;
+    } else {
+      countObj[i][1]++;
+    }
+  }
+}
+// console.log(countObj);
+
+let code = [];
+let reverseCode = [];
+
+for (let line of Object.values(countObj)) {
+  if (line[0] > line[1]) {
+    code.push(1);
+    reverseCode.push(0);
+  } else {
+    code.push(0);
+    reverseCode.push(1);
+  }
+}
+
+const codeToDecimal = parseInt(+String([...code]).replaceAll(",", ""), 2);
+const revCodeToDecimal = parseInt(
+  +String([...reverseCode]).replaceAll(",", ""),
+  2
+);
+
+console.log(codeToDecimal);
+console.log(revCodeToDecimal);
+console.log(codeToDecimal * revCodeToDecimal);
